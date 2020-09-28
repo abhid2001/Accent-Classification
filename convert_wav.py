6b5ab8989a8e8eb64a6d8043817ef96a55332aa0
navya1618@gmail.com <navya1618@gmail.com>
2020-09-28 19:27:12 +0530

    Converted to wav

---
+++ convert_wav.py
@@ -0,0 +1,14 @@
+import os
+from pydub import AudioSegment
+
+path = "Audio"
+os.chdir(path)
+
+audio_files = os.listdir()
+
+for file in audio_files:                                                                         
+    name, ext = os.path.splitext(file)
+    if ext == ".mp3":
+       mp3_sound = AudioSegment.from_mp3(file)
+       #rename them using the old name + ".wav"
+       mp3_sound.export("..\\WavFormat\\{0}.wav".format(name), format="wav")
