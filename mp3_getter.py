801cd84872d566931fa6533106fed4c14c1b17da
Abhinav <dugarabhinav2001@gmail.com>
2020-10-06 17:01:56 +0530

    Removed age and age_onset

--- mp3_getter.py
+++ mp3_getter.py
@@ -85,13 +85,16 @@ def get_speaker_info(start, stop):
 # Extracting data of the required languages from the dataset
 def extract_from_data(langs):
     # Dont execute the code in comments
-    #df1 = pd.read_csv("Data/speaker_info_2920-2940.csv")
+    df1 = pd.read_csv("Data/speaker_info_2920-2940.csv")
     df = pd.read_csv("Data/speaker_info_all.csv")
     df = df[df['native_language'].isin(langs)]
-    #df1 = df1[df1['native_language'].isin(langs)]
-    #final = pd.concat([df, df1], axis=0)
-    df.drop(['Unnamed: 0','age','age_onset'], axis = 1, inplace = True)
-    df.to_csv('Data/final_data.csv')
+    df1 = df1[df1['native_language'].isin(langs)]
+    final = pd.concat([df, df1], axis=0)
+    final.drop(['Unnamed: 0','age','age_onset'], axis = 1, inplace = True)
+    final.to_csv('Data/final_data.csv')
+    #data = pd.read_csv('Data/final_data.csv')
+    #data.drop(['Unnamed: 0','age','age_onset'], axis = 1, inplace = True)
+    #	data.to_csv('Data/final_data.csv')
 
 
 if __name__ == "__main__":
@@ -100,9 +103,9 @@ if __name__ == "__main__":
              'kannada', 'mandarin', 'russian', 'spanish']
     lang_tuple = get_formatted_languages(langs)
     print(lang_tuple)
-    # ('arabic', 194), ('english', 646), ('french', 80), ('german', 42), ('hindi', 34), ('kannada', 9), ('mandarin', 150), ('russian', 81), ('spanish', 228)]
+    #('arabic', 194), ('english', 646), ('french', 80), ('german', 42), ('hindi', 34), ('kannada', 9), ('mandarin', 150), ('russian', 81), ('spanish', 228)]
     print('Downloading now...')
-   # mp3getter(lang_tuple)
+    #mp3getter(lang_tuple)
     #get_speaker_info(1, 2941)
     extract_from_data(langs)  # Extracted Data stored in final_data.csv
     print("DONE!!")
