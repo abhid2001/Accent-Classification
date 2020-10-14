4e189ea829ce35947cee409028f67e050b4e7955
gaurav2699 <gauravsingh2699@gmail.com>
2020-10-14 23:16:59 +0530

    splitted csv file and made segments for mfcc and converted list of language to binary class matrix

--- convert_wav.py
+++ convert_wav.py
@@ -1,12 +1,13 @@
 import os
 from pydub import AudioSegment
+import requests
 
 path = "Audio"
 os.chdir(path)
 
 audio_files = os.listdir()
 
-for file in audio_files:                                                                         
+for file in audio_files:
     name, ext = os.path.splitext(file)
     if ext == ".mp3":
        mp3_sound = AudioSegment.from_mp3(file)
--- mfcc.py
+++ mfcc.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import librosa
 import os
+import multiprocessing
+from sklearn.model_selection import train_test_split
+from keras import utils
 
 DEBUG = True
 SILENCE_THRESHOLD = .01
@@ -15,7 +18,7 @@ def get_wav(file_name):
     Load wav file from disk and down-samples to RATE
     '''
 
-    y, sr = librosa.load('../Audio/{}.wav'.format(file_name))
+    y, sr = librosa.load('Audio/{}.wav'.format(file_name))
     return(librosa.core.resample(y=y,orig_sr=sr,target_sr=RATE, scale=True))
 
 def to_mfcc(wav_array):
@@ -24,5 +27,46 @@ def to_mfcc(wav_array):
     '''
     return(librosa.feature.mfcc(y=wav_array, sr=RATE, n_mfcc=N_MFCC))
 
-wav_array = get_wav('arabic1')
-mfcc = to_mfcc(wav_array)
+
+def split_people(df,test_size=0.2):
+    '''
+    Create train test split of DataFrame
+    '''
+
+    return train_test_split(df['filename'],df['native_language'],test_size=test_size,random_state=1234)
+
+def to_categorical(y):
+    '''
+    Converts list of languages into a binary class matrix
+    '''
+    lang_dict = {}
+    for index,language in enumerate(set(y)):
+        lang_dict[language] = index
+    y = list(map(lambda x: lang_dict[x],y))
+    return utils.to_categorical(y, len(lang_dict))
+
+def make_segments(mfccs,labels):
+    '''
+    Makes segments of mfccs and attaches them to the labels
+    '''
+    segments = []
+    seg_labels = []
+    for mfcc,label in zip(mfccs,labels):
+        for start in range(0, int(mfcc.shape[1] / COL_SIZE)):
+            segments.append(mfcc[:, start * COL_SIZE:(start + 1) * COL_SIZE])
+            seg_labels.append(label)
+    return(segments, seg_labels)
+
+df = pd.read_csv('Data/test.csv')
+X_train, X_test, y_train, y_test = split_people(df)
+pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+X_train = pool.map(get_wav, X_train)
+X_test = pool.map(get_wav, X_test)
+X_train = pool.map(to_mfcc, X_train)
+X_test = pool.map(to_mfcc, X_test)
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+X_train, y_train = make_segments(X_train, y_train)
+X_validation, y_validation = make_segments(X_test, y_test)
+
+print (X_train)
